c67ab45d4799e08a102b3e28b36a6ea067ae6626
Malik Fassi Fihri <fassi.malik@hotmail.com>
2013-06-24 17:02:07 +0200

    models login

--- login/models.py
+++ login/models.py
@@ -4,15 +4,17 @@ from django import forms
 
 class Utilisateur(models.Model):
 	profilBase = models.OneToOneField(User, primary_key=True, related_name='profilBaseInUtilisateur')
-	birthday = models.DateField()
-	#afsbreljked= models.CharField(max_length=7)
-	#competences -> many-to-one-> utilisateur
-    #historique(cours) -> many-to-one -> utilisateur
-	#notification -> many-to-one -> utilisateur
-
+	
 	def __unicode__(self):
 		return (self.profilBase.first_name+" "+self.profilBase.last_name+" @ "+self.profilBase.email)
 
 	def __repr__(self):
 		return self.__unicode__()
 
+class WatchLater(models.Model):
+	ID = models.CharField(max_length=100)
+	relationUtilisateur =models.ForeignKey(Utilisateur) 
+
+class FavoriteArticle(models.Model):
+	ID = models.CharField(max_length=100)
+	relationUtilistateur = models.ForeignKey(Utilisateur)
